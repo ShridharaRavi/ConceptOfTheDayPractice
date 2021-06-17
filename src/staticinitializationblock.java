8dd1c3ef416250fbd95d32dd74e78f97489bc267
shridhar.1si11ec106@gmail.com <shridhar.1si11ec106@gmai.com>
2021-06-17 14:17:04 +0530

    first commit

---
+++ src/staticinitializationblock.java
@@ -0,0 +1,32 @@
+class A
+{
+	static int i;
+	
+	static
+	{
+		System.out.println("from static block");
+	}
+	
+	static void m1()
+	{
+		System.out.println("from M1 ");
+		System.out.println(i);
+	}
+}
+
+
+public class staticinitializationblock {
+	
+	static 
+	{
+		System.out.println("main class static block");
+	}
+	public static void main(String args[])
+	{
+		
+		A.i=100;
+		A.m1();
+		
+	}
+
+}
